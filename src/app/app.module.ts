764654353d17a38cd5d1a2f558683b15b33b2504
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-08-23 15:38:55 -0400

    criado component agregar-editar-persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListPersonasComponent } from './components/list-personas/list-personas.component';
+import { AgregarEditarPersonaComponent } from './components/agregar-editar-persona/agregar-editar-persona.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListPersonasComponent
+    ListPersonasComponent,
+    AgregarEditarPersonaComponent
   ],
   imports: [
     BrowserModule,
