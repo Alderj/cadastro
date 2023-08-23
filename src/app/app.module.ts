353267fda92a45f71357701c5ef8ffa0a45a7b3e
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-08-23 15:37:53 -0400

    criado component list-personas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListPersonasComponent } from './components/list-personas/list-personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPersonasComponent
   ],
   imports: [
     BrowserModule,
