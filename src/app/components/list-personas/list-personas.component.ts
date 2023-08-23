bf95f0e288a16b1bfac88c4003a57de5631ab07f
Alderj <33815682+Alderj@users.noreply.github.com>
2023-08-23 19:38:22 -0400

    update para listPersonas

--- src/app/components/list-personas/list-personas.component.ts
+++ src/app/components/list-personas/list-personas.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const listPersonas: PeriodicElement[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
