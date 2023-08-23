f329c657fb9ea69f50b08e0fd99a8751e56cfec3
Alderj <33815682+Alderj@users.noreply.github.com>
2023-08-23 19:37:36 -0400

    excluido a interface de exemplo do material

--- src/app/components/list-personas/list-personas.component.ts
+++ src/app/components/list-personas/list-personas.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
 const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
