1edf8e186e410ccc94dd6e74f9b26f946ca73e32
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-08-24 09:09:21 -0400

    import MatIconModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -27,7 +28,7 @@ import { MatInputModule } from '@angular/material/input';
     BrowserAnimationsModule,
     MatSlideToggleModule, MatToolbarModule, MatCardModule,
     MatTableModule, MatPaginatorModule, MatSortModule,
-    MatFormFieldModule, MatInputModule
+    MatFormFieldModule, MatInputModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
