240148f9bb624760cf23e6f3cb804f5fd3dcb511
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-08-24 11:08:54 -0400

    import MatDatepickerModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [],
@@ -23,14 +24,14 @@ import { MatSelectModule } from '@angular/material/select';
     MatTableModule, MatPaginatorModule, MatSortModule,
     MatFormFieldModule, MatInputModule, MatIconModule,
     MatTooltipModule, MatButtonModule, MatDialogModule,
-    MatSelectModule
+    MatSelectModule, MatDatepickerModule
 
   ], exports: [
     MatSlideToggleModule, MatToolbarModule, MatCardModule,
     MatTableModule, MatPaginatorModule, MatSortModule,
     MatFormFieldModule, MatInputModule, MatIconModule,
     MatTooltipModule, MatButtonModule, MatDialogModule,
-    MatSelectModule
+    MatSelectModule, MatDatepickerModule
   ]
 })
 export class SharedModule { }
