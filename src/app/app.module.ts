a1c22420b6b768839e44a397f62774acdd422d41
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-08-24 09:46:59 -0400

    importado o shared module dentro de appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListPersonasComponent } from './components/list-personas/list-personas.component';
 import { AgregarEditarPersonaComponent } from './components/agregar-editar-persona/agregar-editar-persona.component';
-
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatButtonModule } from '@angular/material/button';
-
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -28,10 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule, MatToolbarModule, MatCardModule,
-    MatTableModule, MatPaginatorModule, MatSortModule,
-    MatFormFieldModule, MatInputModule, MatIconModule,
-    MatTooltipModule, MatButtonModule
+    SharedModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
