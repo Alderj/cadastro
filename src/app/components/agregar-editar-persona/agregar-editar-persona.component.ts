77f6d17f5b8050d3acce29e6d72252cd8e7c804e
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-08-24 10:37:15 -0400

    metodo cancelar() adicionado

--- src/app/components/agregar-editar-persona/agregar-editar-persona.component.ts
+++ src/app/components/agregar-editar-persona/agregar-editar-persona.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-agregar-editar-persona',
   templateUrl: './agregar-editar-persona.component.html',
   styleUrls: ['./agregar-editar-persona.component.css']
 })
-export class AgregarEditarPersonaComponent {
+export class AgregarEditarPersonaComponent implements OnInit{
+
+  constructor( public dialogRef: MatDialogRef<AgregarEditarPersonaComponent>){}
+
+  ngOnInit(): void {
+  }
+
+  cancelar(){
+    this.dialogRef.close();
+  }
 
 }
